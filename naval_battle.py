d33382f39be0de89dfac3774123291b48a87e7e5
Alban MARTEL <alban.martel@fms-ea.com>
2026-07-20 11:23:01 +0200

    Ajout du constructeur init de la class BOAT

---
+++ naval_battle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#-*-coding: utf-8 -*/-
+
+from typing import ClassVar
+from dataclasses import dataclass
+
+@dataclass
+class Boat():
+    def __init__(self, type, startposition, endposition, gameover):
+        self.type = type
+        self.startposition = startposition
+        self.endposition = endposition
+        self.squares = []
+        self.gameover = gameover
+
+
