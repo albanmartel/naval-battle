592a5024222b15d49a3493f565b262a86967554d
Alban MARTEL <alban.martel@fms-ea.com>
2026-07-20 12:51:14 +0200

    Ajout de la fonction separate_letters_digits(position)

--- Positions.py
+++ Positions.py
@@ -10,3 +10,15 @@ class Positions():
     def __init__(self, startposition: str, endposition: str):
         self.startposition = startposition
         self.endposition = endposition
+
+
+def separate_letters_digits(position: str) -> tuple:
+    numbers = []
+    letters = []
+    for character in position:
+        if character.isdigit():
+            numbers.append(character)
+        else:
+            letters.append(character)
+
+    return int(numbers), letters
