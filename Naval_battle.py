6075dca17ceba9e61d01e9bc350d6129f4c694bc
Alban Martel <alban.f.j.martel@laposte.net>
2026-07-21 22:51:45 +0200

    Définition de plusieurs getters et setters à l'aide des décorateurs

--- Naval_battle.py
+++ Naval_battle.py
@@ -9,7 +9,7 @@ import Boat
 class Naval_battle():
     ##### Les Constantes ######
     boats_coordinates = (
-        ("aircraft carrier", "B2", "B2"),
+        ("aircraft carrier", "B2", "F2"),
         ("cruiser", "A4", "A7"),
         ("destroyer", "C5", "C7"),
         ("submarine", "H5", "J5"),
@@ -18,8 +18,20 @@ class Naval_battle():
 
     def __init__(self, size):
         self.size = size
+        all_boats = []
         for elem in self.boats_coordinates:
             boat = Boat.Boat(elem[0], elem[1], elem[2], False)
-        self.boats = boat.get_boats()
+            all_boats.append(boat)
+
+        self.boats = all_boats
         self.turn = 0
         self.gameover = False
+
+    # --- GETTER ---
+    @property
+    def boats(self):
+        return self._boats
+
+    @boats.setter
+    def boats(self, boats):
+        self._boats = boats
