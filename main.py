a395a74f7cce4e5ed553de868384f8a83da42f96
Alban Martel <alban.f.j.martel@laposte.net>
2026-07-21 22:52:34 +0200

    Modifier le main pour affichers les bâteaux instanciés

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import Naval_battle
 
 def main()-> None:
     bataille_navale = Naval_battle.Naval_battle(10)
+    bateaux = bataille_navale.boats
+    for bateau in bateaux:
+        print(f"\"{bateau.type}\": {bateau.originals_squares}")
 
 
 if __name__ == "__main__":
